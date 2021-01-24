efc2a346f136199dc7f1bfa0c6eeae1b750aaaf2
docopeland <doc22@cornell.edu>
2021-01-24 03:10:09 +0000

    question 4

--- Python3Programming/course3/project.py
+++ Python3Programming/course3/project.py
@@ -66,3 +66,16 @@ def get_related_titles(movieList):
                 fullList.append(relMovie)
     return fullList
 
+# Your next task will be to fetch data from OMDB. The documentation for the API is at https://www.omdbapi.com/
+# Define a function called get_movie_data. It takes in one parameter which is a string that should represent the title
+# of a movie you want to search. The function should return a dictionary with information about that movie.
+# Again, use requests_with_caching.get(). For the queries on movies that are already in the cache, you won’t need an api
+# key. You will need to provide the following keys: t and r. As with the TasteDive cache, be sure to only include those
+# two parameters in order to extract existing data from the cache.
+def get_movie_data(movie):
+    baseurl = "http://www.omdbapi.com/"
+    parameters = {"t": movie, "r": "json"}
+    req = requests_with_caching.get(baseurl, params = parameters)
+    res = req.json()
+    return res
+
